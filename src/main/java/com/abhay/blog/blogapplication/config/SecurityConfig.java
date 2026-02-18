255c13bbd98f666eded2420633025fa88ba58ebb
Abhay <abhaypvt2903@gmail.com>
2026-02-19 00:55:44 +0530

    Add modification in dependency and some fix

--- src/main/java/com/abhay/blog/blogapplication/config/SecurityConfig.java
+++ src/main/java/com/abhay/blog/blogapplication/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.abhay.blog.blogapplication.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +24,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+
 public class SecurityConfig{
     
     @Autowired
@@ -56,9 +59,9 @@ public class SecurityConfig{
         
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/api/v1/auth/**","/api/users/**").permitAll()
+            .requestMatchers(HttpMethod.GET).permitAll()
             .anyRequest()
             .authenticated()
-    
         )
         .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
         
