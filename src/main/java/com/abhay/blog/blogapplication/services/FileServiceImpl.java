ae05ee24f2345e997f34f7ddd0eaeb678bacf4db
Abhay <abhaypvt2903@gmail.com>
2026-03-16 00:34:38 +0530

    Add changes in fileServiceImpl for storing images in s3 storage instead of localstorage

--- src/main/java/com/abhay/blog/blogapplication/services/FileServiceImpl.java
+++ src/main/java/com/abhay/blog/blogapplication/services/FileServiceImpl.java
@@ -1,45 +1,57 @@
 package com.abhay.blog.blogapplication.services;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
 @Service
-public class FileServiceImpl implements FileService{
+public class FileServiceImpl implements FileService {
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    private final S3Client s3Client = S3Client.builder()
+            .region(Region.of("eu-north-1"))
+            .build();
 
     @Override
     public String uploadImage(String path, MultipartFile file) throws IOException {
-        
+
         String name = file.getOriginalFilename();
-
         String randomID = UUID.randomUUID().toString();
-        String fileName1 = randomID.concat(name.substring(name.lastIndexOf(".")));
+        String fileName = randomID.concat(name.substring(name.lastIndexOf(".")));
 
-        String filePath = path + File.separator + fileName1;
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(fileName)
+                        .contentType(file.getContentType())
+                        .build(),
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize())
+        );
 
-        File f = new File(path);
-        if(!f.exists()){
-            f.mkdir();
-        }
-
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-        return fileName1;
+        return fileName;
     }
 
     @Override
-    public InputStream getResorce(String path, String fileName) throws FileNotFoundException {
-    
-        String fullPath = path + File.separator + fileName;
-        InputStream is = new FileInputStream(fullPath);
-        return is;
-    }
+    public InputStream getResorce(String path, String fileName) {
 
+        return s3Client.getObject(
+                GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(fileName)
+                        .build()
+        );
+    }
 }
